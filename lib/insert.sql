8ba9b97e4887afd25480d11d992e4a13daee6153
zanhe <zhertica@gmail.com>
2020-06-11 16:26:59 -0700

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears VALUES (1, "Mr. Chocolate", 32, "M", "brown","docile", "TRUE");
+INSERT INTO bears VALUES (2, "Rowdy", 22, "M", "brown","rowdy", "TRUE");
+INSERT INTO bears VALUES (3, "Tabitha", 45, "F", "black","quiet", "FALSE");
+INSERT INTO bears VALUES (4, "Sergeant Brown", 33, "M", "dark-brown","serious", "TRUE");
+INSERT INTO bears VALUES (5, "Melissa", 24, "F", "brown","reserved", "TRUE");
+INSERT INTO bears VALUES (6, "Grinch", 22, "M", "black","reserved", "TRUE");
+INSERT INTO bears VALUES (7, "Wendy", 41, "F", "dark-brown","docile", "TRUE");
+INSERT INTO bears VALUES (8, NULL, NULL, "M", "dark-brown","aggressive", "TRUE");
